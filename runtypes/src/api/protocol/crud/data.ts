76e33b83a2d415bb0bbe503c4fa5d10c4ab9b3f3
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2023-08-19 10:39:46 +0300

    #2 Adding code for runtypes framework. Also updat…
    …ing README.

--- runtypes/src/api/protocol/crud/data.ts
+++ runtypes/src/api/protocol/crud/data.ts
@@ -2,13 +2,13 @@
  * @file This file contains the validators which will be used to validate the HTTP-protocol related inputs and outputs when calling the endpoints.
  */
 
-import * as data from "@ty-ras/data-zod"; // This package will be present in both BE and FE.
-import * as t from "zod";
+import * as data from "@ty-ras/data-runtypes"; // This package will be present in both BE and FE.
+import * as t from "runtypes";
 
-export const thing = t.object({
-  payload_string: t.string(),
-  payload_timestamp: t.string().datetime(), // Zod does not have easy way to handle transformations like io-ts has with encoders/decoders. For simplicity sake, we fallback to string.
-  id: t.string(),
+export const thing = t.Record({
+  payload_string: t.String,
+  payload_timestamp: t.String, // Runtypes does not have easy way to handle transformations like io-ts has with encoders/decoders. For simplicity sake, we fallback to string.
+  id: t.String,
 });
 
 /**
